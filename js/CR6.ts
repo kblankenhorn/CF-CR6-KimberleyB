a8c193a643f450878004366a02581bdb86ef495b
Kimberley Blankenhorn <kvandlen@yahoo.com>
2020-10-16 21:35:20 +0200

    added date

--- js/CR6.ts
+++ js/CR6.ts
@@ -1,7 +1,7 @@
 const travelArr:Array<any>=[];
 
 class Travel{
-	
+	created: string;
 	country: string;
 	city: string;
 	place: string;
@@ -11,8 +11,8 @@ class Travel{
 	
 	
 
-	constructor(country, city, place, type, address, img){
-		
+	constructor(created, country, city, place, type, address, img){
+		this.created = created;
 		this.country = country;
 		this.city = city;
 		this.place = place;
@@ -29,6 +29,7 @@ class Travel{
 		<div class="card-deck-row text-center col-sm-11 col-md-5 bg-dark col-lg-2 m-4 p-1 text-white justify-content-between w-100 rounded" id="Places">
 		<img src="${this.img}" class= "card-img-top card-img-fluid my-2 d-none d-md-block">
 		<div class="card-body">
+		<p> ${this.created}</p>
 		<h6 class="card-title mb-2">${this.country}</h6>
 		<p class="address">${this.place}</p>
 		<p class="address">${this.address}, ${this.city}</p>
@@ -45,8 +46,8 @@ class Restaurants extends Travel {
 	website: string;
 	phone: string;
 
-	constructor(country: string, city: string, place: string, type: string, address: string, img: string, food: string, website: string, phone: string){
-		super(country, city, place, type, address, img);
+	constructor(created: string, country: string, city: string, place: string, type: string, address: string, img: string, food: string, website: string, phone: string){
+		super(created, country, city, place, type, address, img);
 		this.food = food;
 		this.website = website;
 		this.phone = phone;
@@ -56,7 +57,8 @@ class Restaurants extends Travel {
 		return `
 		<div class="card-deck-row text-center col-sm-11 col-md-5 bg-dark col-lg-2 m-4 p-1 text-white justify-content-between w-100 rounded"  id="Places">
 		<img src="${this.img}" class= "card-img-top card-img-fluid my-2 d-none d-md-block">
-		<div class="card-body"
+		<div class="card-body">
+		<p> ${this.created}</p>
 		<h6 class="card-title mb-2">${this.country}</h6>
 		<p class="address">${this.place}</p>
 		<p class="address">${this.address}, ${this.city}</p>
@@ -72,8 +74,8 @@ class Entertaining extends Travel {
 	website: string;
 	price: "string";
 
-	constructor(country: string, city: string, place: string, type: string, address: string, img: string, activity:string, website: string, price: string, date: string){
-		super(country, city, place, type, address, img);
+	constructor(created: string, country: string, city: string, place: string, type: string, address: string, img: string, activity:string, website: string, price: string, date: string){
+		super(created, country, city, place, type, address, img);
 		this.activity = activity;
 		this.website = website;
 		this.price = price;
@@ -85,6 +87,7 @@ class Entertaining extends Travel {
 		<div class="card-deck-row text-center col-sm-11 col-md-5 bg-dark col-lg-2 m-4 p-1 text-white justify-content-between w-100 rounded"  id="Places">
 		<img src="${this.img}" class= "card-img-top card-img-fluid my-2 d-none d-md-block">
 		<div class="card-body">
+		<p> ${this.created}</p>
 		<h6 class="card-title mb-2">${this.country}</h6>
 		<p class="address">${this.place}</p>
 		<p class="address">${this.address}, ${this.city}</p>
@@ -98,12 +101,12 @@ class Entertaining extends Travel {
 }
 
 
-var tokyo = new Travel("Japan", "Kyoto", "Fushimi Inari-taisha Shrine", "Sightseeing", "68 Fukakusa Yabunouchicho, Fushimi Ward, Kyoto, 612-0882, Japan", "./img/torii.jpg");
-var sushi = new Restaurants("Japan", "Kyoto", "Sushi Iwa", "Sushi Restaurant", "〒600-8155 Kyoto, Shimogyo Ward, 角", "./img/sushi1.jpg", "Sushi", "http://www.sushiiwa.jp/en/", "+81 75-371-9303");
-var entertaining = new Entertaining("Japan", "Kyoto", "Maiko Theater", "Theater", "302-2 Daikokucho, Higashiyama Ward, Kyoto, 605-0832, Japan", "./img/geisha.jpg", "Entertainment", "https://mykyotomachiya.com/japanese-language/", "€150", "Saturday 17th July 2021");
-var thailand = new Travel("Thailand", "Chiang Mai", "Wat Phra Singh", "Sightseeing", "2 Samlarn Rd, Phra Sing, Mueang Chiang Mai District, Chiang Mai 50280, Thailand", "./img/chiang-mai.jpg");
-var padThai = new Restaurants("Thailand", "Chiang Mai", "Cooking Love", "Thai Food", "18/7 Rachadamnoen Road, Soi 1, Chiang Mai 50200 Thailand", "./img/pad-thai.jpg", "Pad Thai", "https://www.facebook.com/GoodFood4You/", "+66 52 002 748");
-var show = new Entertaining("Thailand", "Chian Mai", "Chaing Mai Cultural Performance", "Performance Art", "333 somewhere st, Chiang Mai 00000 Thailand", "./img/actor.jpg", "Entertainment", "https://www.lonelyplanet.com/thailand/activities/chiang-mai-traditional-khan-toke-meal-cultural-performance/a/pa-act/v-9592P129/357592", "€100", "Saturday 14 August 2021");
+var tokyo = new Travel("Created: 6/6/20", "Japan", "Kyoto", "Fushimi Inari-taisha Shrine", "Sightseeing", "68 Fukakusa Yabunouchicho, Fushimi Ward, Kyoto, 612-0882, Japan", "./img/torii.jpg");
+var sushi = new Restaurants("Created: 6/6/20","Japan", "Kyoto", "Sushi Iwa", "Sushi Restaurant", "〒600-8155 Kyoto, Shimogyo Ward, 角", "./img/sushi1.jpg", "Sushi", "http://www.sushiiwa.jp/en/", "+81 75-371-9303");
+var entertaining = new Entertaining("Created: 6/6/20","Japan", "Kyoto", "Maiko Theater", "Theater", "302-2 Daikokucho, Higashiyama Ward, Kyoto, 605-0832, Japan", "./img/geisha.jpg", "Entertainment", "https://mykyotomachiya.com/japanese-language/", "€150", "Saturday 17th July 2021");
+var thailand = new Travel("Created: 6/6/20","Thailand", "Chiang Mai", "Wat Phra Singh", "Sightseeing", "2 Samlarn Rd, Phra Sing, Mueang Chiang Mai District, Chiang Mai 50280, Thailand", "./img/chiang-mai.jpg");
+var padThai = new Restaurants("Created: 6/6/20","Thailand", "Chiang Mai", "Cooking Love", "Thai Food", "18/7 Rachadamnoen Road, Soi 1, Chiang Mai 50200 Thailand", "./img/pad-thai.jpg", "Pad Thai", "https://www.facebook.com/GoodFood4You/", "+66 52 002 748");
+var show = new Entertaining("Created: 6/6/20","Thailand", "Chian Mai", "Chaing Mai Cultural Performance", "Performance Art", "333 somewhere st, Chiang Mai 00000 Thailand", "./img/actor.jpg", "Entertainment", "https://www.lonelyplanet.com/thailand/activities/chiang-mai-traditional-khan-toke-meal-cultural-performance/a/pa-act/v-9592P129/357592", "€100", "Saturday 14 August 2021");
 
 $(document).ready(function(){
 	for(let i in travelArr){
